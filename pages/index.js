9760fe721ef0532530be2b3600e6509ce73485bf
jaewon <korkrip11@gmail.com>
2021-10-27 19:45:55 +0900

    update tag

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Item from '../components/Item'
-import SkeletonItemfrom from '../components/skeletonItem'
-import { API, graphqlOperation } from 'aws-amplify'
-import { Container, Box } from '@mui/material'
+import SkeletonItem from '../components/skeletonItem'
+import { API } from 'aws-amplify'
+import { Container } from '@mui/material'
 import { listPosts } from '../graphql/queries'
 
 const Page = () => {
@@ -13,8 +13,11 @@ const Page = () => {
     setLoading(true)
 
     try {
-      const { data } = await API.graphql(graphqlOperation(listPosts))
-      console.log(data)
+      const { data } = await API.graphql({
+        query: listPosts,
+        authMode: 'AWS_IAM',
+      })
+
       setPosts(data.listPosts.items)
     } catch (err) {
       console.log('error : ', err)
@@ -45,11 +48,10 @@ const Page = () => {
               title={post.title}
               markDown={post.markDown}
               createdAt={post.createdAt}
+              description={post.description}
             />
           ))
-        : [...Array(3)].map((_, i) => (
-            <SkeletonItemfrom key={`skeleton_${i}`} />
-          ))}
+        : [...Array(3)].map((_, i) => <SkeletonItem key={`skeleton_${i}`} />)}
     </Container>
   )
 }
