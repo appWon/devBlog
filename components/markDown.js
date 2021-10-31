f1350472b081265cf2d777c0d5e8bdaacc71ea03
jaewon <korkrip11@gmail.com>
2021-11-01 02:15:04 +0900

    feat markdown

--- components/markDown.js
+++ components/markDown.js
@@ -4,7 +4,11 @@ import 'github-markdown-css/github-markdown.css'
 
 export const MarkDown = ({ children }) => {
   return (
-    <ReactMarkDown className="markdown-body p-3" remarkPlugins={[gfm]} c>
+    <ReactMarkDown
+      className="markdown-body p-3"
+      remarkPlugins={[gfm]}
+      skipHtml={true}
+    >
       {children}
     </ReactMarkDown>
   )
